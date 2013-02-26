38cf0ad069f68b50e1473744e6c446ad5bb0dfb4
javieret <javiereskivel@gmail.com>
2013-02-25 18:54:41 -0600

    Scroll with no end

---
+++ example.js
@@ -0,0 +1,7 @@
+$(document).bind('scroll', function() {
+	if (($(window).innerHeight() + $(window).scrollTop()) >= $("body").height()) 
+	{
+		loadTips();//function you will call when this scroll has reach the end
+	}
+});
+		
